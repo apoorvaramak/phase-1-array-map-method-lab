974b66addf3917e5592b3a6a70201637dc750fae
Apoorva Ramakrishnan <apoorvar@comcast.net>
2021-06-29 12:52:57 -0500

    commit tree ada930de1bc6f7872c6e149e6753643ca22e40de

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
 ];
 
+function capitalFirst(str){
+  console.log(str.charAt(0).toUpperCase())
+  return str.charAt(0).toUpperCase(); 
+}
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(str){
+    let strArray = str.split(' '); 
+    for(let i = 0; i < strArray.length; i++){
+      let string = strArray[i]; 
+      strArray[i] = string.charAt(0).toUpperCase() + string.slice(1); 
+    }
+    return strArray.join(' '); 
+  });
 }
